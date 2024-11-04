fd953981045e59e4a866ae55987a4e136374296d
Hugo Saldarriaga <hugosmm2000@gmail.com>
2024-11-04 16:32:46 -0500

    feat: Add new dependencies like lombok and change the tasks for jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,17 +3,31 @@ plugins {
 }
 
 group = "io.github.imecuadorian"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
+val lombokVersion = "1.18.34"
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    archiveBaseName.set("generic-library")
+    archiveVersion.set("0.0.1")
+    archiveClassifier.set("")
 }
